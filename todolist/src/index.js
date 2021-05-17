69a153bbf63025eacd181f5adde161acf9844f4f
Pablo Ferrer-Dalmau <pablo@Pablo>
2021-05-17 11:48:45 +0200

    React

---
+++ todolist/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './TodoList';
+import './index.css';
+
+ReactDOM.render(
+<div>
+<h1> Todo List </h1>
+<TodoList />
+</div>
+document.getElementById('root'))
+);
